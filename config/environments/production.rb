4cc21257b05bd696aaf71da147440e5712ad82a2
tipay <tecinfpay@gmail.com>
2015-07-13 21:09:01 +0000

    jja

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,13 +10,14 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
   
   config.action_mailer.smtp_settings= {
-  address: 'smtp.gmail.com',
-  port:                 587,
-  domain: 'gmail.com',
-  authentication: 'plain',
-  enable_starttls_auto: true,
-  user_name: 'javaee2015@gmail.com',
-  password: '1234567890tipay'
+    address: 'smtp.gmail.com',
+    port:                 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: 'javaee2015@gmail.com',
+    password: '1234567890tipay',
+    :enable_starttls_auto => true 
   }
   
   # Code is not reloaded between requests.
